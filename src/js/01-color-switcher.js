7f23c948b91a90a2313dd771b059d8d559bd4fc0
Oleh Halytskyi <oleh.halytskyy@gmail.com>
2022-10-30 21:43:55 +0200

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+let getEl = selector => document.querySelector(selector)
+const body = getEl('body')
+let interval = null;
 
+const buttonStart = getEl('button[data-start]');
+const buttonStop = getEl('button[data-stop]');
+
+buttonStop.setAttribute("disabled",'');
+
+buttonStart.addEventListener('click', intervalChangeColor);
+buttonStop.addEventListener('click', intervalDeleteClick);
+
+function changeBodyColor(value) {
+    body.style.backgroundColor = value; 
+}
+
+let bodyColor = function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function intervalChangeColor() {
+      interval = setInterval(() => {
+        changeBodyColor(bodyColor())
+      }, 1000);
+    buttonStart.setAttribute("disabled", '');
+    buttonStop.removeAttribute("disabled");
+}
+    
+function intervalDeleteClick() {
+    clearInterval(interval);
+    buttonStop.setAttribute("disabled", '');
+    buttonStart.removeAttribute("disabled");
+}
