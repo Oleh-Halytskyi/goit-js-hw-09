a9be6ed10e588244b9db3beeae67d14227a6a1e8
Oleh Halytskyi <oleh.halytskyy@gmail.com>
2022-11-05 10:40:03 +0200

    add task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,53 @@
+import Notiflix from 'notiflix';
+let getEl = selector => document.querySelector(selector)
+
+const form = getEl('.form')
+let firstDelay = null;
+let step = null;
+let amount = null;
+let delay = null;
+let position = 1;
+
+
+form.onsubmit = async (evt) => {
+  evt.preventDefault();
+
+  let formData = new FormData(form);
+  firstDelay = Number(formData.get('delay'))
+  step = Number(formData.get('step'))
+  amount = Number(formData.get('amount'))
+  
+  delay = firstDelay;
+  
+  for (let i = 1; i <= amount; i += 1) {
+
+    createPromise(i, delay).then(( position, delay) => {
+      (Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`));
+    })
+    .catch((position, delay) => {
+      (Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
+    })
+    .finally(() => {
+        form.reset();
+        firstDelay = null;
+        step = null;
+        amount = null
+      })
+    
+    delay += step;
+   }
+  
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise ((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      } else {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+      }
+    }, delay)
+    })
 }
